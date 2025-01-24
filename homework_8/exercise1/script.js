be73f7a0ea8386769071bb22990114286a1ea97b
SS24A2 <ana.janevska21@yahoo.com>
2025-01-24 12:58:10 +0100

    added hw8-ex.1

---
+++ homework_8/exercise1/script.js
@@ -0,0 +1,30 @@
+const usersList = document.getElementById('users-list')
+
+async function displayData() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        data.forEach((user) => {
+            const individualList = document.createElement('ul')
+            individualList.classList.add('individual_list')
+            usersList.appendChild(individualList)
+            const name = document.createElement('li')
+            name.textContent = `Name: ${user.name}`
+            individualList.appendChild(name)
+            name.classList.add('property')
+            const phone = document.createElement('li')
+            phone.textContent = `Phone number: ${user.phone}`
+            individualList.appendChild(phone)
+            phone.classList.add('property')
+            const company = document.createElement('li')
+            company.textContent = `Company name: ${user.company.name}`
+            individualList.appendChild(company)
+            company.classList.add('property')
+        })
+    }
+    catch (error) {
+        console.error('Error occurred!', error)
+    }
+}
+
+displayData()
